2c3e6d9b9c8a1f8b4f9ef0d93fd083a214a208dd
Reupen Shah <reupen@users.noreply.github.com>
2015-11-01 12:12:14 +0000

    add Chrome, Fx to karma config

--- karma.conf.js
+++ karma.conf.js
@@ -1,21 +1,7 @@
 module.exports = function (config) {
     config.set({
 
-        browsers: ['PhantomJS_custom'],
-
-        customLaunchers: {
-            'PhantomJS_custom': {
-                base: 'PhantomJS',
-                options: {
-                    windowName: 'my-window',
-                    settings: {
-                        webSecurityEnabled: false
-                    }
-                },
-                flags: ['--load-images=true'],
-                debug: true
-            }
-        },
+        browsers: ['PhantomJS', 'Firefox', 'Chrome'],
 
         phantomjsLauncher: {
             exitOnResourceError: true
