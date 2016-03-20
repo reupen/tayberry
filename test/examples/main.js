cf3c9cf0dbff7537d1942bb031441808c1be7d8e
Reupen Shah <reupen@users.noreply.github.com>
2016-03-20 20:39:53 +0000

    unify test charts

--- test/examples/main.js
+++ test/examples/main.js
@@ -42,194 +42,286 @@ var standardTestCharts = {
         }]
     },
     'chart2': {
-    title: {
-        text: 'Contribution to revenue growth'
-    },
-    xAxis: {
-        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'],
         title: {
-            text: 'Month'
-        }
-    },
-    yAxis: {
-        title: {
-            text: 'Impact'
+            text: 'Contribution to revenue growth'
         },
-        labelFormat: 'currency'
-    },
-    barPlot: {
-        mode: 'stacked'
-    },
-    tooltips: {
-        shared: true
-    },
-    series: [{
-        data: [10, 9, 8, 7, 6, 5, 4, 3, 3, 0, -3, -5],
-        name: 'Frequency'
-    }, {
-        data: [-5, -6, -8, -10, -9, -7, -7, -3, -2, 2, 1, 3],
-        name: 'Price'
-    }, {
-        data: [2, 1, 3, -1, -3, -3, -4, 3, 4, 7, 3, 2],
-        name: 'Special offers'
-    }, {
-        data: [5, 6, 7, 6, 2, 2, 10, 6, 1, 2, -3, -6],
-        name: 'Transaction size'
-    }]
-}, 'chart3': {
-    title: {
-        text: 'UK household sizes'
-    },
-    yAxis: {
-        labelFormat: 'percentage'
-    },
-    xAxis: {
-        categories: ['1', '2', '3', '4', '5+']
-    },
-    labels: {
-        enabled: true
-    },
-    series: [{
-        data: [.132, .290, .224, .180, .173],
-        name: '1961'
-    }, {
-        data: [.306, .341, .156, .129, .069],
-        name: '2011'
-    }]
-
-}, 'chart4': {
-    title: {
-        text: 'UK population by region (mid-2013)'
-    },
-    xAxis: {
-        categories: ['North East',
-            'North West',
-            'Yorkshire and The Humber',
-            'East Midlands',
-            'West Midlands',
-            'East',
-            'London',
-            'South East',
-            'South West'
-        ]
-    },
-    swapAxes: true,
-    yAxis: {
-        labelFormatter: function (val) {
-            return (val / 1000000).toFixed(1) + 'm';
-        }
-    },
-    series: [{
-        data: [2610481,
-            7103261,
-            5337711,
-            4598431,
-            5674712,
-            5954316,
-            8416543,
-            8792766,
-            5377596],
-        name: 'Population (people)'
-    }]
-}, 'chart5': {
-    title: {
-        text: 'UK household sizes'
-    },
-    yAxis: {
-        labelFormat: 'percentage'
-    },
-    xAxis: {
-        categories: ['1', '2', '3', '4', '5+'],
-        title: {
-            text: '123',
-            font: {
-                style: 'italic bold'
+        xAxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'],
+            title: {
+                text: 'Month'
             }
-        }
-    },
-    labels: {
-        enabled: true
-    },
-    plotType: 'line',
-    series: [{
-        data: [.132, .290, .224, .180, .173],
-        name: '1961',
-        colour: '#6FE87B'
-    }, {
-        data: [.306, .341, .156, .129, .069],
-        name: '2011',
-        colour: '#FFAB51'
-    }]
-
-}, 'chart6': {
-    title: {
-        text: 'UK household sizes'
-    },
-    yAxis: [{
-        labelFormat: 'percentage'
-    }, {
-        labelFormat: 'percentage'
-    }],
-    xAxis: [{
-        min: 0,
-        max: 10,
-        type: 'linear',
+        },
+        yAxis: {
+            title: {
+                text: 'Impact'
+            },
+            labelFormat: 'currency'
+        },
+        barPlot: {
+            mode: 'stacked'
+        },
+        tooltips: {
+            shared: true
+        },
+        series: [{
+            data: [10, 9, 8, 7, 6, 5, 4, 3, 3, 0, -3, -5],
+            name: 'Frequency'
+        }, {
+            data: [-5, -6, -8, -10, -9, -7, -7, -3, -2, 2, 1, 3],
+            name: 'Price'
+        }, {
+            data: [2, 1, 3, -1, -3, -3, -4, 3, 4, 7, 3, 2],
+            name: 'Special offers'
+        }, {
+            data: [5, 6, 7, 6, 2, 2, 10, 6, 1, 2, -3, -6],
+            name: 'Transaction size'
+        }]
+    }, 'chart3': {
         title: {
-            text: '123',
-            font: {
-                style: 'italic bold'
-            }
-        }
-    }],
-    labels: {
-        enabled: true
-    },
-    plotType: 'line',
-    series: [{
-        data: [[0, .132], [3, .290], [4, .224], [6, .180], [10, .173]],
-        name: '1961',
-        colour: '#6FE87B'
-    }, {
-        data: [[1, .306], [2, .341], [3, .156], [4, .129], [5, .069]],
-        name: '2011',
-        colour: '#FFAB51',
-        plotType: 'bar',
-        yAxisIndex: 1
-    }]
+            text: 'UK household sizes'
+        },
+        yAxis: {
+            labelFormat: 'percentage'
+        },
+        xAxis: {
+            categories: ['1', '2', '3', '4', '5+']
+        },
+        labels: {
+            enabled: true
+        },
+        series: [{
+            data: [.132, .290, .224, .180, .173],
+            name: '1961'
+        }, {
+            data: [.306, .341, .156, .129, .069],
+            name: '2011'
+        }]
 
-}, 'chart7': {
-    title: {
-        text: 'Random data - 1000 points'
-    },
-    yAxis: {},
-    xAxis: {
-        type: 'linear',
+    }, 'chart4': {
         title: {
-            text: '123',
-            font: {
-                style: 'italic bold'
+            text: 'UK population by region (mid-2013)'
+        },
+        xAxis: {
+            categories: ['North East',
+                'North West',
+                'Yorkshire and The Humber',
+                'East Midlands',
+                'West Midlands',
+                'East',
+                'London',
+                'South East',
+                'South West'
+            ]
+        },
+        swapAxes: true,
+        yAxis: {
+            labelFormatter: function (val) {
+                return (val / 1000000).toFixed(1) + 'm';
             }
-        }
-    },
-    plotType: 'line',
-    series: [{
-        data: randomData1,
-        name: '1961',
-        colour: '#6FE87B'
-    }, {
-        data: randomData2,
-        name: '2011',
-        colour: '#FFAB51'
-    }]
+        },
+        series: [{
+            data: [2610481,
+                7103261,
+                5337711,
+                4598431,
+                5674712,
+                5954316,
+                8416543,
+                8792766,
+                5377596],
+            name: 'Population (people)'
+        }]
+    }, 'chart5': {
+        title: {
+            text: 'UK household sizes'
+        },
+        yAxis: {
+            labelFormat: 'percentage'
+        },
+        xAxis: {
+            categories: ['1', '2', '3', '4', '5+'],
+            title: {
+                text: '123',
+                font: {
+                    style: 'italic bold'
+                }
+            }
+        },
+        labels: {
+            enabled: true
+        },
+        plotType: 'line',
+        series: [{
+            data: [.132, .290, .224, .180, .173],
+            name: '1961',
+            colour: '#6FE87B'
+        }, {
+            data: [.306, .341, .156, .129, .069],
+            name: '2011',
+            colour: '#FFAB51'
+        }]
 
-}
+    }, 'chart6': {
+        title: {
+            text: 'UK household sizes'
+        },
+        yAxis: [{
+            labelFormat: 'percentage'
+        }, {
+            labelFormat: 'percentage'
+        }],
+        xAxis: [{
+            min: 0,
+            max: 10,
+            type: 'linear',
+            title: {
+                text: '123',
+                font: {
+                    style: 'italic bold'
+                }
+            }
+        }],
+        labels: {
+            enabled: true
+        },
+        plotType: 'line',
+        series: [{
+            data: [[0, .132], [3, .290], [4, .224], [6, .180], [10, .173]],
+            name: '1961',
+            colour: '#6FE87B'
+        }, {
+            data: [[1, .306], [2, .341], [3, .156], [4, .129], [5, .069]],
+            name: '2011',
+            colour: '#FFAB51',
+            plotType: 'bar',
+            yAxisIndex: 1
+        }]
+
+    }, 'chart7': {
+        title: {
+            text: 'Random data - 1000 points'
+        },
+        yAxis: {},
+        xAxis: {
+            type: 'linear',
+            title: {
+                text: '123',
+                font: {
+                    style: 'italic bold'
+                }
+            }
+        },
+        plotType: 'line',
+        series: [{
+            data: randomData1,
+            name: '1961',
+            colour: '#6FE87B'
+        }, {
+            data: randomData2,
+            name: '2011',
+            colour: '#FFAB51'
+        }]
+
+    }
 };
 
-Object.keys(standardTestCharts).forEach(function(key) {
-    createdCharts[key] = Tayberry.create(key, standardTestCharts[key]);
-});
+var legendTestCharts = {
+    'chart1': {
+        title: {
+            text: 'UK household sizes'
+        },
+        yAxis: {
+            labelFormat: 'percentage'
+        },
+        xAxis: {
+            categories: ['1', '2', '3', '4', '5+']
+        },
+        labels: {
+            enabled: true
+        },
+        legend: {
+            placement: 'bottom'
+        },
+        series: [{
+            data: [.132, .290, .224, .180, .173],
+            name: '1961 abcd'
+        }, {
+            data: [.306, .341, .156, .129, .069],
+            name: '2011 abcd'
+        }, {
+            data: [.306, .341, .156, .129, .069],
+            name: '2011 abcd'
+        }, {
+            data: [.306, .341, .156, .129, .069],
+            name: '2011 abcd'
+        }, {
+            data: [.306, .341, .156, .129, .069],
+            name: '2011 abcd'
+        }, {
+            data: [.306, .341, .156, .129, .069],
+            name: '2011 abcd'
+        }, {
+            data: [.306, .341, .156, .129, .069],
+            name: '2011 abcd'
+        }, {
+            data: [.306, .341, .156, .129, .069],
+            name: '2011 abcd'
+        }, {
+            data: [.306, .341, .156, .129, .069],
+            name: '2011 abcd'
+        }]
 
-$('#chart1-add').on('click', function() {
+    }, 'chart2': {
+        title: {
+            text: 'Contribution to revenue growth'
+        },
+        xAxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'],
+            title: {
+                text: 'Month'
+            }
+        },
+        yAxis: {
+            title: {
+                text: 'Impact'
+            },
+            labelFormat: 'currency'
+        },
+        barPlot: {
+            mode: 'stacked'
+        },
+        tooltips: {
+            shared: true
+        },
+        series: [{
+            data: [10, 9, 8, 7, 6, 5, 4, 3, 3, 0, -3, -5],
+            name: 'Frequency'
+        }, {
+            data: [-5, -6, -8, -10, -9, -7, -7, -3, -2, 2, 1, 3],
+            name: 'Price'
+        }, {
+            data: [2, 1, 3, -1, -3, -3, -4, 3, 4, 7, 3, 2],
+            name: 'Special offers'
+        }, {
+            data: [5, 6, 7, 6, 2, 2, 10, 6, 1, 2, -3, -6],
+            name: 'Transaction size'
+        }]
+    }
+};
+
+function createChartSet(chartSet) {
+    Object.keys(createdCharts).forEach(function (key) {
+        createdCharts[key].destroy();
+    });
+
+    createdCharts = {};
+
+    Object.keys(chartSet).forEach(function (key) {
+        createdCharts[key] = Tayberry.create(key, $.extend(true, {}, chartSet[key]));
+    });
+}
+
+$('#chart1-add').on('click', function () {
     createdCharts['chart1'].addSeries({
         data: [15, 9, 23, 26, 35, 44, 30, 70, 58, 75, 85, 107, 112, 120, 144, 158, 184, 196, 197, 190, 202, 222, 221, 226, 209, 221, 191, 198, 189, 168, 151, 142, 121, 112, 80, 82, 69, 49, 51, 51, 34, 27, 20, 11, 11, 12, 4, 3, 1, 4],
         name: 'Refunds',
@@ -237,10 +329,19 @@ $('#chart1-add').on('click', function() {
     });
 });
 
-$('#chart2-add').on('click', function() {
+$('#chart2-add').on('click', function () {
     createdCharts['chart2'].addSeries({
         data: [3, 4, 3, 5, 8, 9, 1, 3, 3, 5, 3, 6],
         name: 'Refunds'
     });
 });
 
+$('#set-general').on('click', function () {
+    createChartSet(standardTestCharts);
+});
+
+$('#set-legend').on('click', function () {
+    createChartSet(legendTestCharts);
+});
+
+createChartSet(standardTestCharts);
