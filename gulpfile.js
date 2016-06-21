592b4a154f94d4df19c4bfb390bffae7467e77f3
Reupen Shah <reupen@users.noreply.github.com>
2016-06-21 21:14:24 +0100

    Switch from gulp-rollup to rollup-stream

--- gulpfile.js
+++ gulpfile.js
@@ -8,15 +8,16 @@ var rename = require('gulp-rename');
 var eslint = require('gulp-eslint');
 var Server = require('karma').Server;
 
-var rollup = require('gulp-rollup');
+var rollup = require('rollup-stream');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
 var rollupbabel = require('rollup-plugin-babel');
 
 gulp.task('lint', function () {
     return gulp.src(['src/**/*.js', 'test/**/*.js'])
         .pipe(eslint())
         .pipe(eslint.format())
-        .pipe(eslint.failOnError())
-        ;
+        .pipe(eslint.failOnError());
 });
 
 gulp.task('test', function (done) {
@@ -35,20 +36,23 @@ gulp.task('test:ci', function (done) {
 });
 
 gulp.task('build', function () {
-    gulp.src('./src/tayberry.js', {read: false})
-        .pipe(rollup({
-            sourceMap: true,
-            plugins: [
-                rollupbabel({
-                    presets: ['es2015-rollup']
-                })
-            ],
-            format: 'umd',
-            moduleName: 'Tayberry'
-        }))
+    return rollup({
+        entry: './src/tayberry.js',
+        sourceMap: true,
+        plugins: [
+            rollupbabel({
+                presets: ['es2015-rollup']
+            })
+        ],
+        format: 'umd',
+        moduleName: 'Tayberry'
+    })
+        .pipe(source('tayberry.js', './src'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({loadMaps: true}))
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('./dist/'))
-        .pipe(filter(['*', '!**/*.js.map']))
+        .pipe(filter(['**', '!**/*.js.map']))
         .pipe(rename({suffix: '.min'}))
         .pipe(sourcemaps.init({loadMaps: true}))
         .pipe(uglify())
