af0fc73b1996b616ba5d06e73bbb64c004fa59fc
Reupen Shah <reupen@users.noreply.github.com>
2020-07-19 20:16:35 +0100

    Update Gulpfile for Gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,34 @@
 'use strict';
 
-var gulp = require('gulp');
-var filter = require('gulp-filter');
-var uglify = require('gulp-uglify');
-var sourcemaps = require('gulp-sourcemaps');
-var rename = require('gulp-rename');
-var eslint = require('gulp-eslint');
-var Server = require('karma').Server;
+const {dest, series, src, watch: watch_} = require('gulp');
+const filter = require('gulp-filter');
+const uglify = require('gulp-uglify');
+const sourcemaps = require('gulp-sourcemaps');
+const rename = require('gulp-rename');
+const eslint = require('gulp-eslint');
+const Server = require('karma').Server;
 
-var rollup = require('rollup-stream');
-var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
-var rollupbabel = require('rollup-plugin-babel');
+const rollup = require('rollup-stream');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+const rollupbabel = require('rollup-plugin-babel');
 
-gulp.task('lint', function () {
-    return gulp.src(['src/**/*.js', 'test/**/*.js'])
+function lint() {
+    return src(['src/**/*.js', 'test/**/*.js'])
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failOnError());
-});
+}
 
-gulp.task('test', function (done) {
+function test(cb) {
     new Server({
         configFile: __dirname + '/karma.conf.js',
         singleRun: true
-    }, done).start();
-});
+    }).start();
+    cb();
+}
 
-gulp.task('test:ci', function (done) {
-    new Server({
-        configFile: __dirname + '/karma.conf.js',
-        singleRun: true,
-    }, done).start();
-});
-
-gulp.task('build', function () {
+function build() {
     return rollup({
         input: './src/tayberry.js',
         sourcemap: true,
@@ -51,20 +45,23 @@ gulp.task('build', function () {
         .pipe(buffer())
         .pipe(sourcemaps.init({loadMaps: true}))
         .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('./dist/'))
+        .pipe(dest('./dist/'))
         .pipe(filter(['**', '!**/*.js.map']))
         .pipe(rename({suffix: '.min'}))
         .pipe(sourcemaps.init({loadMaps: true}))
         .pipe(uglify())
         .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('./dist/'));
-});
+        .pipe(dest('./dist/'));
+}
 
-gulp.task('default', ['lint', 'build', 'test']);
+function watch() {
+    return watch_(['src/*'], series(lint, build));
+}
 
-gulp.task('ci', ['lint', 'build', 'test:ci']);
+exports.lint = lint;
+exports.test = test;
+exports.build = build;
+exports.watch = series(lint, build, watch);
 
-gulp.task('watch', ['default'], function () {
-    gulp.watch('src/*', ['lint', 'build']);
-});
-
+exports.default = series(lint, build, test);
+exports.ci = series(lint, build, test);
